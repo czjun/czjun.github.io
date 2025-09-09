69d81e3cac0f5e6831650662611b2b0c6d7c9bbe
czjun <2692595304@qq.com>
2025-09-09 14:45:20 +0800

    Site updated: 2025-09-09 14:45:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/26/hello-world/","2025/09/02/plane-的经验之谈/","2025/08/27/论自我介绍的重要性/","2025/08/26/刚刚是0-现在是1/"];function toRandomPost(){
+var posts=["2025/08/26/hello-world/","2025/09/02/plane-的经验之谈/","2025/08/27/论自我介绍的重要性/","2025/08/26/刚刚是0-现在是1/","2025/09/09/部署中遇到的问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
